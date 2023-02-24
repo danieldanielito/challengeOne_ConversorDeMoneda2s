ff3a407eb026c69c1e930661b7a27d88d73a0fd9
danieldanielito <113646615+danieldanielito@users.noreply.github.com>
2023-02-24 19:12:50 -0300

    arreglos

--- src/convDeMon/Conversor.java
+++ src/convDeMon/Conversor.java
@@ -35,65 +35,36 @@ public class Conversor {
 					curso.addAlumno("de yen a peso", new CambioNombre(monto * 1.44, "pesos"));
 					curso.addAlumno("de peso a wun", new CambioNombre(monto * 6.64, "wun"));
 					curso.addAlumno("de wun a peso", new CambioNombre(monto / 6.64, "pesos"));
-                    
-                    
-					for (HashMap.Entry<String, CambioNombre> entry : curso.getCambMap().entrySet()) {
-						System.out.printf(entry.getKey() + " " + entry.getValue() + "\n");
-																			
-					}
 
 					ArrayList<String> curso5 = new ArrayList();
 					curso5 = (ArrayList<String>) curso.getCamb();
 					m = curso5.size();
 					String[] cambios = new String[m];
 					for (int i = 0; i < curso5.size(); i++) {
-
-						// System.out.println(curso5.get(i));
 						cambios[i] = curso5.get(i);
-
-						// System.out.println(cambios[i]);
 					}
 
-					/*
-					 * cambios =
-					 * {"de dolar a peso","de peso a dolar","de euro a peso","de peso a euro",
-					 * "de libra esterlina a peso", "de peso a libras esterlinas","de yen a peso",
-					 * "de peso a yen","de wun a peso","de peso a wun" };
-					 */
 					String mone = (String) JOptionPane.showInputDialog(null, "elija el cambio a realizar", "cambio",
 							JOptionPane.QUESTION_MESSAGE, null, cambios, cambios[0]);
 
-					//montocambiado.get(mone);
-					//System.out.println(montocambiado.get(mone));
 					JOptionPane.showMessageDialog(null, "son " + curso.getCambMap().get(mone));
 
 				} else {
 					Double monto = Double.parseDouble(JOptionPane.showInputDialog("ingrese los kilometros a cambiar"));
-
 					Curso1 curso = new Curso1();
-
 					curso.addAlumno("de kilometro a metro ", new CambioNombre(monto * 1000, "metros"));
 					curso.addAlumno("de kilometro a centimetro ", new CambioNombre(monto * 100000, "centimetros"));
 					curso.addAlumno("de kilometro a milimetro ", new CambioNombre(monto * 1000000, "milimetros"));
-					curso.addAlumno("de kilometro a decimetro ", new CambioNombre(monto * 10000,"decimetros"));
-
+					curso.addAlumno("de kilometro a decimetro ", new CambioNombre(monto * 10000, "decimetros"));
 					ArrayList<String> curso5 = new ArrayList();
 					curso5 = (ArrayList<String>) curso.getCamb();
 					m = curso5.size();
 					String[] cambios = new String[m];
 					for (int i = 0; i < curso5.size(); i++) {
-
-						// System.out.println(curso5.get(i));
 						cambios[i] = curso5.get(i);
-
-						// System.out.println(cambios[i]);
 					}
-
-					// String[] cambios = {"de kilometro a metro ","de kilometro a centimetro ","de
-					// kilometro a milimetro "};
 					String mone = (String) JOptionPane.showInputDialog(null, "elija el cambio a realizar", "cambio",
 							JOptionPane.QUESTION_MESSAGE, null, cambios, cambios[0]);
-
 					JOptionPane.showMessageDialog(null, "son " + curso.getCambMap().get(mone));
 				}
 				salida = JOptionPane.showConfirmDialog(null, " desea volver al menu?");
@@ -104,8 +75,6 @@ public class Conversor {
 						"ud ha decidido cancelar la operacion, desea volver al menu?");
 			}
 		} while (salida == 0);
-
 		JOptionPane.showMessageDialog(null, "salir");
-
 	}
 }
--- src/convDeMon/Curso1.java
+++ src/convDeMon/Curso1.java
@@ -6,45 +6,28 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-
-
-
-
 public class Curso1 {
-	private Collection <String> camb= new ArrayList();
-	//private Collection <Alumno> alumno= new LinkedHashSet();
-	private Map<String, CambioNombre>cambMap=  new HashMap();
-	
-	
-	
+	private Collection<String> camb = new ArrayList();
+	private Map<String, CambioNombre> cambMap = new HashMap();
+
 	public Curso1() {
 	}
-
-
 
 	public Collection<String> getCamb() {
 		return camb;
 	}
 
-
-
 	public void setCamb(Collection<String> camb) {
 		this.camb = camb;
 	}
-
-
 
 	public Map<String, CambioNombre> getCambMap() {
 		return cambMap;
 	}
 
-
-
 	public void setCambMap(Map<String, CambioNombre> cambMap) {
 		this.cambMap = cambMap;
 	}
-
-
 
 	public void addAlumno(String de_a, CambioNombre nombre) {
 		this.camb.add(de_a);
